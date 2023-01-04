29b1fb41f144de2ab7206fb22b1d34b32270c0f1
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-04 16:57:16 +0530

    fix:build-error

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,6 +1,6 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext, CreateTRPCNext } from '@trpc/next';
-import type { AppRouter } from '../src/pages/api/trpc/[trpc]';
+import type { AppRouter } from '../src/server/routers/_app';
 
 function getBaseUrl() {
   if (typeof window !== 'undefined') {

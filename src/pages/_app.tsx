eadeb578c7190d49c4634891190d6f1bf8164c3a
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-05 21:16:46 +0530

    add:app.tsx::meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../../styles/globals.css';
 import React from 'react';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider as JotaiProvider } from 'jotai';
 import type { AppProps } from 'next/app';
@@ -10,6 +11,12 @@ const queryClient = new QueryClient();
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta
+          property='og:image'
+          content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjy8ZRHjNBs1EgrDGihwNV03homRyqVVPLQ&usqp=CAU'
+        />
+      </Head>
       <JotaiProvider>
         <Component {...pageProps} />
       </JotaiProvider>

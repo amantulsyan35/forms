e7278e89ad2c7edb780273f0eb72e12c0b2ba090
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-05 21:51:55 +0530

    change:image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
+import { AiFillPlayCircle } from 'react-icons/ai';
 
 const Home = () => {
   return (
     <main
-      style={{ backgroundImage: `url('./context_bg.webp')` }}
+      style={{ backgroundImage: `url('./context.jpg')` }}
       className='min-h-screen  bg-cover bg-no-repeat bg-center flex flex-column gap-4 justify-center items-center'
     >
       <Link href='/context/new' legacyBehavior>
         <a>
-          <button className='p-4 rounded-md pointer-cursor border-2  text-white hover:bg-black  hover:border-none'>
+          <AiFillPlayCircle className=' h-[128px] w-[128px] text-[#999] hover:text-[#fff] text-[40px] mt-[-75px]' />
+          {/* <button className='p-4 rounded-md pointer-cursor border-2  text-white hover:bg-black  hover:border-none'>
             Create Context
-          </button>
+          </button> */}
         </a>
       </Link>
     </main>

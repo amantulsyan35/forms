dbb621f5205e176194c5fb82a9c03726c672e5c9
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-05 21:13:34 +0530

    remove:gif

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,8 +16,8 @@ export default function Document() {
           content='context is the key to narrative storytelling'
         />
         <meta
-          property='og:image/gif'
-          content='https://mir-s3-cdn-cf.behance.net/project_modules/disp/31a97258737059.5a07705b4b565.gif'
+          property='og:image'
+          content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjy8ZRHjNBs1EgrDGihwNV03homRyqVVPLQ&usqp=CAU'
         />
       </Head>
       <body>

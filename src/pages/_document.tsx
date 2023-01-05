acfdf8bff9a6da10ec6a7ecc8ec2dabd854bc8f1
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-05 21:08:59 +0530

    fix:bug:meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,11 @@ export default function Document() {
       <Head>
         <meta property='og:url' content='your url' />
         <meta property='og:type' content='website' />
-        <meta property='og:title' content='./context_bg.webp' />
-        <meta name='twitter:card' content='./context_bg.webp' />
+        <meta property='og:title' content='Context' />
+        <meta
+          name='twitter:card'
+          content='https://mir-s3-cdn-cf.behance.net/project_modules/disp/31a97258737059.5a07705b4b565.gif'
+        />
         <meta
           property='og:description'
           content='context is the key to narrative storytelling'

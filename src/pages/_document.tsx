89c2d1e209dd4c80e1f3ea4023b1c47e7660897d
Aman Tulsyan <amantulsyan35@gmail.com>
2023-01-06 20:53:43 +0530

    add:meta-changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,18 +7,12 @@ export default function Document() {
         <meta property='og:url' content='your url' />
         <meta property='og:type' content='website' />
         <meta property='og:title' content='Context' />
-        <meta
-          name='twitter:card'
-          content='https://mir-s3-cdn-cf.behance.net/project_modules/disp/31a97258737059.5a07705b4b565.gif'
-        />
+        <meta name='twitter:card' content='context_bg' />
         <meta
           property='og:description'
           content='context is the key to narrative storytelling'
         />
-        <meta
-          property='og:image'
-          content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjy8ZRHjNBs1EgrDGihwNV03homRyqVVPLQ&usqp=CAU'
-        />
+        <meta property='og:image' content='context_bg' />
       </Head>
       <body>
         <Main />
